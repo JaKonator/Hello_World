b186be377a132cf77fc121745527627eca07fd72
JaKonator <stephan-alois.jachs@stud.unileoben.ac.at>
2016-08-23 09:18:42 +0200

    Hello World
    This is my first upload to GitHub and it is my first Java application

---
+++ hello_world.java
@@ -0,0 +1,13 @@
+/*Hello World
+*/
+
+public class hello_world{
+
+public static void main(String[] args) {
+
+    //Ausgabe
+   System.out.println("So beginnt beinahe jeder Progammierkurs");
+   System.out.println("So \n\tbeginnt \nbeinahe \r jeder Programmierkurs");
+   System.out.println("\nDas ist ein Test");
+  }
+} //Ende der Klasse main
